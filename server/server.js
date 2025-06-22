7424d68800d42be8dee892e7d585c644072fe6d6
ajharul <mdajharulislam327@gmail.com>
2025-06-22 17:25:06 +0600

    add new  origin

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB()
 await connectCloudinary()
 
 //alowed origins for CORS
-const allowedOrigins = ['http://localhost:5173']; 
+const allowedOrigins = ['http://localhost:5173',' https://green-cart-jet.vercel.app']; 
 
 // middleware config
 app.use(express.json());
