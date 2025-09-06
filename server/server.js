a693a54aa8a2532738a97515aa74935bf06a2d07
ajharul <mdajharulislam327@gmail.com>
2025-09-06 23:46:11 +0600

    add cors

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
-import 'dotenv/config'
+import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
@@ -10,24 +10,26 @@ import ProductRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-await connectDB()
-await connectCloudinary()
+await connectDB();
+await connectCloudinary();
 
-//alowed origins for CORS
-const allowedOrigins = ['http://localhost:5173','https://green-cart-jet.vercel.app']; 
+// Allowed origins for CORS
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://green-cart-jet.vercel.app',
+  'https://grocery-app-1-tbtg.onrender.com/'
+];
 
-// middleware config
+// Middleware config
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- // This parses JSON bodies for requests
-
 app.use(cookieParser());
 app.use(cors({
-  origin: allowedOrigins, 
+  origin: allowedOrigins,
   credentials: true,
 }));
 
@@ -35,16 +37,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/api/user',userRouter);
+app.use('/api/user', userRouter);
 app.use('/api/seller', sellerRouter);
 app.use('/api/product', ProductRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/address', addressRouter);
 app.use('/api/order', orderRouter);
- 
 
-    
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
