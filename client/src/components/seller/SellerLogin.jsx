ce4968ba6f5423bfd7ce2761b31df33782beb5a8
ajharul <mdajharulislam327@gmail.com>
2025-09-19 01:10:49 +0600

    currection seller login css

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -37,19 +37,19 @@ const SellerLogin = () => {
   return !isSeller && (
     <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600 '>
 
-    <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200 ">
+    <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-500 ">
 
         <p className='text-xl font-medium m-auto'><span className='text-primary'>Seller </span>Login</p>
     
     <div className='w-full'>
     <p>Email</p>
     <input onChange={(e)=>setEmail(e.target.value)} value={email}
-     type="email" placeholder='Enter your email'  className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary " required/>
+     type="email" placeholder='Enter your email'  className="border border-gray-500 rounded w-full p-2 mt-1 outline-primary " required/>
     </div>
     <div className='w-full'>
     <p>Password</p>
     <input onChange={(e)=>setPassword(e.target.value)} value={password}
-     type="password" placeholder='Enter your password' className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary " required/>
+     type="password" placeholder='Enter your password' className="border border-gray-500 rounded w-full p-2 mt-1 outline-primary " required/>
     </div>
     <button className='bg-primary text-white w-full py-2 rounded-md cursor-pointer '>Login</button>
     </div>
